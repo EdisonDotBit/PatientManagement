2dcbbba2a83a01c603b23e9d31362b1a3741ffb2
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-12 15:00:48 +0800

    Update data annotations in PatientEntity.cs

--- EL/PatientEntity.cs
+++ EL/PatientEntity.cs
@@ -14,14 +14,17 @@ namespace EL
 
         [Required(ErrorMessage = "Patient name is required.")]
         [StringLength(50, ErrorMessage = "Patient name cannot exceed 50 characters.")]
+        [RegularExpression(@"^(?!\s*$)[a-zA-Z\s'-]+$", ErrorMessage = "Patient name cannot be blank and can only contain letters, spaces, hyphen, and apostrophe.")]
+
         public string Patient { get; set; }
 
         [Required(ErrorMessage = "Drug name is required.")]
         [StringLength(50, ErrorMessage = "Drug name cannot exceed 50 characters.")]
+        [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "Drug name cannot be blank or whitespace.")]
         public string Drug { get; set; }
 
         [Required(ErrorMessage = "Dosage is required.")]
-        [Range(0.01, 9999.99, ErrorMessage = "Dosage must be greater than 0.")]
+        [Range(0.0001, 9999.9999, ErrorMessage = "Dosage must be greater than 0.")]
         public decimal Dosage { get; set; }
         public DateTime ModifiedDate { get; set; }
     }
