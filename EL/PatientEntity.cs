b7d480286c89423450bc811c4eff7308a2573555
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-16 10:29:38 +0800

    Add data annotation in PatientEntity.cs

--- EL/PatientEntity.cs
+++ EL/PatientEntity.cs
@@ -15,7 +15,6 @@ namespace EL
         [Required(ErrorMessage = "Patient name is required.")]
         [StringLength(50, ErrorMessage = "Patient name cannot exceed 50 characters.")]
         [RegularExpression(@"^(?!\s*$)[a-zA-Z\s'-]+$", ErrorMessage = "Patient name cannot be blank and can only contain letters, spaces, hyphen, and apostrophe.")]
-
         public string Patient { get; set; }
 
         [Required(ErrorMessage = "Drug name is required.")]
@@ -25,6 +24,7 @@ namespace EL
 
         [Required(ErrorMessage = "Dosage is required.")]
         [Range(0.0001, 9999.9999, ErrorMessage = "Dosage must be greater than 0.")]
+        [RegularExpression(@"^\d+(\.\d{1,4})?$", ErrorMessage = "Dosage can have up to 4 decimal places only.")]
         public decimal Dosage { get; set; }
         public DateTime ModifiedDate { get; set; }
     }
