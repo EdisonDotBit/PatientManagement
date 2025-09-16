7c255ff8e709f5f3ebfa95f65686004a62a41a30
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-16 10:29:51 +0800

    Update RouteConfig.cs

--- PatientManagement/App_Start/RouteConfig.cs
+++ PatientManagement/App_Start/RouteConfig.cs
@@ -14,9 +14,22 @@ namespace PatientManagement
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+            name: "EditWithoutID",
+            url: "Patient/Edit",
+            defaults: new { controller = "Home", action = "RouteNotFound" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+           
+            routes.MapRoute(
+                name: "NotFound",
+                url: "{*url}",
+                defaults: new { controller = "Home", action = "RouteNotFound" }
             );
         }
     }
