20891d41f8cbc77690ee92f38deb6c8fa851e9fc
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-16 10:29:07 +0800

    Add PatientValidateDAL.cs
    This serves as a dedicated dal method for validators and duplicate checkers that is separate from the CRUD DAL methods.

---
+++ DAL/PatientValidateDAL.cs
@@ -0,0 +1,48 @@
+﻿using EL;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class PatientValidateDAL : PatientContextDAL
+    {
+        public bool IsDuplicate(PatientEntity patientEntity)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = @"SELECT COUNT(*) FROM PatientDetails 
+                                 WHERE Patient = @Patient AND Drug = @Drug AND CAST(ModifiedDate AS DATE) = CAST(@ModifiedDate AS DATE)";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Patient", patientEntity.Patient);
+                cmd.Parameters.AddWithValue("@Drug", patientEntity.Drug);
+                cmd.Parameters.AddWithValue("@ModifiedDate", patientEntity.ModifiedDate);
+
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+
+        public bool IsUpdateDuplicate(PatientEntity patientEntity)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = @"SELECT COUNT(*) FROM PatientDetails 
+                         WHERE Patient = @Patient AND Drug = @Drug 
+                         AND CAST(ModifiedDate AS DATE) = CAST(@ModifiedDate AS DATE)
+                         AND ID <> @ID";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Patient", patientEntity.Patient);
+                cmd.Parameters.AddWithValue("@Drug", patientEntity.Drug);
+                cmd.Parameters.AddWithValue("@ModifiedDate", patientEntity.ModifiedDate);
+                cmd.Parameters.AddWithValue("@ID", patientEntity.ID);
+
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+    }
+}
