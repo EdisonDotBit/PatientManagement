7f75733f84223f5c4be1f49a3a92f08b0b4b0bca
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-16 10:28:30 +0800

    Update PatientDAL.cs
    Require all PatientDAL methods to return a boolean that the BLL methods can use for certain logic and validations.
    
    Another, separate the CRUD DAL method from the Duplicate DAL methods similar what we did to BLL.

--- DAL/PatientDAL.cs
+++ DAL/PatientDAL.cs
@@ -1,28 +1,23 @@
 ﻿using EL;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 
 namespace DAL
 {
-    public class PatientDAL
+    public class PatientDAL : PatientContextDAL
     {
-        private readonly string connectionString = ConfigurationManager.ConnectionStrings["PatientManagement"].ConnectionString;
-
         public List<PatientEntity> GetPatients()
         {
-
             List<PatientEntity> patients = new List<PatientEntity>();
-
 
             using (SqlConnection con = new SqlConnection(connectionString))
             {
+                con.Open();
                 string query = "SELECT * FROM PatientDetails";
                 SqlCommand cmd = new SqlCommand(query, con);
 
-                con.Open();
                 SqlDataReader dr = cmd.ExecuteReader();
 
                 while (dr.Read())
@@ -40,10 +35,11 @@ namespace DAL
             return patients;
         }
 
-        public void CreatePatient(PatientEntity patientEntity)
+        public bool CreatePatient(PatientEntity patientEntity)
         {
             using (SqlConnection con = new SqlConnection(connectionString))
             {
+                con.Open();
                 string query = @"INSERT INTO PatientDetails (Patient, Drug, Dosage, ModifiedDate)
                                  VALUES (@Patient, @Drug, @Dosage, @ModifiedDate)";
 
@@ -53,33 +49,16 @@ namespace DAL
                 cmd.Parameters.AddWithValue("@Dosage", patientEntity.Dosage);
                 cmd.Parameters.AddWithValue("@ModifiedDate", patientEntity.ModifiedDate);
 
-                con.Open();
-                cmd.ExecuteNonQuery();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
             }
         }
 
-        public bool IsDuplicate(PatientEntity patientEntity)
+        public bool EditPatient(PatientEntity patientEntity)
         {
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-                string query = @"SELECT COUNT(*) FROM PatientDetails 
-                                 WHERE Patient = @Patient AND Drug = @Drug AND CAST(ModifiedDate AS DATE) = CAST(@ModifiedDate AS DATE)";
-
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@Patient", patientEntity.Patient);
-                cmd.Parameters.AddWithValue("@Drug", patientEntity.Drug);
-                cmd.Parameters.AddWithValue("@ModifiedDate", patientEntity.ModifiedDate);
-
                 con.Open();
-                int count = (int)cmd.ExecuteScalar();
-                return count > 0;
-            }
-        }
-
-        public void EditPatient(PatientEntity patientEntity)
-        {
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
                 string query = @"UPDATE PatientDetails 
                          SET Patient = @Patient, Drug = @Drug, Dosage = @Dosage, ModifiedDate = @ModifiedDate
                          WHERE ID = @ID";
@@ -90,43 +69,23 @@ namespace DAL
                 cmd.Parameters.AddWithValue("@Dosage", patientEntity.Dosage);
                 cmd.Parameters.AddWithValue("@ModifiedDate", patientEntity.ModifiedDate);
                 cmd.Parameters.AddWithValue("@ID", patientEntity.ID);
-
-                con.Open();
-                cmd.ExecuteNonQuery();
+                
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
             }
         }
 
-        public bool IsUpdateDuplicate(PatientEntity patientEntity)
-        {
-            using (SqlConnection con = new SqlConnection(connectionString))
-            {
-                string query = @"SELECT COUNT(*) FROM PatientDetails 
-                         WHERE Patient = @Patient AND Drug = @Drug 
-                         AND CAST(ModifiedDate AS DATE) = CAST(@ModifiedDate AS DATE)
-                         AND ID <> @ID";
-
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@Patient", patientEntity.Patient);
-                cmd.Parameters.AddWithValue("@Drug", patientEntity.Drug);
-                cmd.Parameters.AddWithValue("@ModifiedDate", patientEntity.ModifiedDate);
-                cmd.Parameters.AddWithValue("@ID", patientEntity.ID);
-
-                con.Open();
-                int count = (int)cmd.ExecuteScalar();
-                return count > 0;
-            }
-        }
-
-        public void DeletePatient(int ID)
+        public bool DeletePatient(int ID)
         {
             using (SqlConnection con = new SqlConnection(connectionString)) 
             {
+                con.Open();
                 string query = "DELETE FROM PatientDetails WHERE ID = @ID";
                 SqlCommand cmd = new SqlCommand(query, con);
                 cmd.Parameters.AddWithValue("@ID", ID);
 
-                con.Open();
-                cmd.ExecuteNonQuery();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
             }
         }
     }
