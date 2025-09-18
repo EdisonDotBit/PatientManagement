f0249bf15d6b62200c9bdc6453932f1551fc00b0
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-18 10:18:50 +0800

    Refactor database operations to use stored procedures
    The methods `GetPatients`, `CreatePatient`, `EditPatient`, and `DeletePatient` have been updated to utilize stored procedures (`spGetPatients`, `spCreatePatient`, `spEditPatient`, and `spDeletePatient`). This refactor improves security and maintainability by separating SQL logic from application code.

--- DAL/PatientDAL.cs
+++ DAL/PatientDAL.cs
@@ -1,7 +1,6 @@
 ﻿using EL;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Data.SqlClient;
 
 namespace DAL
@@ -11,81 +10,83 @@ namespace DAL
         public List<PatientEntity> GetPatients()
         {
             List<PatientEntity> patients = new List<PatientEntity>();
-
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-                con.Open();
-                string query = "SELECT * FROM PatientDetails";
-                SqlCommand cmd = new SqlCommand(query, con);
-
-                SqlDataReader dr = cmd.ExecuteReader();
-
-                while (dr.Read())
+                using(SqlCommand cmd = new SqlCommand("spGetPatients", con))
                 {
-                    patients.Add(new PatientEntity
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    con.Open();
+               
+                    using (SqlDataReader dr = cmd.ExecuteReader())
                     {
-                        ID = (int)dr["ID"],
-                        Patient = dr["Patient"].ToString(),
-                        Drug = dr["Drug"].ToString(),
-                        Dosage = (decimal)dr["Dosage"],
-                        ModifiedDate = (DateTime)dr["ModifiedDate"]
-                    });
+                        while (dr.Read())
+                        {
+                            patients.Add(new PatientEntity
+                            {
+                                ID = (int)dr["ID"],
+                                Patient = dr["Patient"].ToString(),
+                                Drug = dr["Drug"].ToString(),
+                                Dosage = (decimal)dr["Dosage"],
+                                ModifiedDate = (DateTime)dr["ModifiedDate"]
+                            });
+                        }
+                    }
                 }
             }
             return patients;
         }
-
         public bool CreatePatient(PatientEntity patientEntity)
         {
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-                con.Open();
-                string query = @"INSERT INTO PatientDetails (Patient, Drug, Dosage, ModifiedDate)
-                                 VALUES (@Patient, @Drug, @Dosage, @ModifiedDate)";
+                using (SqlCommand cmd = new SqlCommand("spCreatePatient", con))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@Patient", patientEntity.Patient);
-                cmd.Parameters.AddWithValue("@Drug", patientEntity.Drug);
-                cmd.Parameters.AddWithValue("@Dosage", patientEntity.Dosage);
-                cmd.Parameters.AddWithValue("@ModifiedDate", patientEntity.ModifiedDate);
+                    cmd.Parameters.AddWithValue("@Patient", patientEntity.Patient);
+                    cmd.Parameters.AddWithValue("@Drug", patientEntity.Drug);
+                    cmd.Parameters.AddWithValue("@Dosage", patientEntity.Dosage);
+                    cmd.Parameters.AddWithValue("@ModifiedDate", patientEntity.ModifiedDate);
 
-                int rowsAffected = cmd.ExecuteNonQuery();
-                return rowsAffected > 0;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
             }
         }
-
         public bool EditPatient(PatientEntity patientEntity)
         {
             using (SqlConnection con = new SqlConnection(connectionString))
             {
-                con.Open();
-                string query = @"UPDATE PatientDetails 
-                         SET Patient = @Patient, Drug = @Drug, Dosage = @Dosage, ModifiedDate = @ModifiedDate
-                         WHERE ID = @ID";
+               using (SqlCommand cmd = new SqlCommand("spEditPatient", con))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@Patient", patientEntity.Patient);
-                cmd.Parameters.AddWithValue("@Drug", patientEntity.Drug);
-                cmd.Parameters.AddWithValue("@Dosage", patientEntity.Dosage);
-                cmd.Parameters.AddWithValue("@ModifiedDate", patientEntity.ModifiedDate);
-                cmd.Parameters.AddWithValue("@ID", patientEntity.ID);
-                
-                int rowsAffected = cmd.ExecuteNonQuery();
-                return rowsAffected > 0;
+                    cmd.Parameters.AddWithValue("@Patient", patientEntity.Patient);
+                    cmd.Parameters.AddWithValue("@Drug", patientEntity.Drug);
+                    cmd.Parameters.AddWithValue("@Dosage", patientEntity.Dosage);
+                    cmd.Parameters.AddWithValue("@ModifiedDate", patientEntity.ModifiedDate);
+                    cmd.Parameters.AddWithValue("@ID", patientEntity.ID);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
             }
         }
-
         public bool DeletePatient(int ID)
         {
             using (SqlConnection con = new SqlConnection(connectionString)) 
             {
-                con.Open();
-                string query = "DELETE FROM PatientDetails WHERE ID = @ID";
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@ID", ID);
+               using (SqlCommand cmd = new SqlCommand("spDeletePatient", con))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ID", ID);
 
-                int rowsAffected = cmd.ExecuteNonQuery();
-                return rowsAffected > 0;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
             }
         }
     }
