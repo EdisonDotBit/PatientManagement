34b84e04d8e0454a998010bf61f26de1718fb8bc
Edison Lati Jr. <junior.lati@gmail.com>
2025-09-30 08:51:01 +0800

    Add HttpErrorController.cs

---
+++ PatientManagement/Controllers/HttpErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PatientManagement.Controllers
+{
+    public class HttpErrorController : Controller
+    {
+        // GET: HttpError
+        public ActionResult RouteNotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+    }
+}
